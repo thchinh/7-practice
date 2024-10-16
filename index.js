e89a316c5d2775915910667da10ee7dc93a174fb
CHTR <chinh.truonghuu@galliker.com>
2024-10-16 21:33:26 +0700

    Init Commit of demo with socket.io

---
+++ index.js
@@ -0,0 +1,21 @@
+// Tuần 5-1
+// Socket.io
+    // - Getting Started with Socket.io 
+    // - Creating the Chat App Project
+    // - Socket.io Events
+    // - Socker.io Events Challenge
+    // - Broadcasting Events
+    // - Sharing Your Location
+    // - Event Acknowledgements
+    // - Form and Button States
+    // - Rendering Messages
+    // - Rendering Location Messages
+    // - Working with Time
+    // - Timestamps for Location Messages
+    // - Styling the Chat App
+    // - Join Page
+    // - Socker.io Rooms
+    // - Storing Users
+    // - Tracking Users Joining and Leaving
+    // - Sending Messages to Rooms
+    // - Rendering User List
